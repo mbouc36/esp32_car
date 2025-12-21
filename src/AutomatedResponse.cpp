9f4934e09a2a8b66b662da784ced502845cf2698
mbouc36 <mbouc36@gmail.com>
2025-12-21 12:06:58 -0500

    setup pins for motor driver controller

--- src/AutomatedResponse.cpp
+++ src/AutomatedResponse.cpp
@@ -5,7 +5,7 @@
 #define EEP_PIN 15 
 
 /* ULT Defines for PWM */
-#define PWN_PIN 2
+#define PWM_PIN 33
 #define CHN     0
 #define FRQ     1000
 #define PWM_BIT   8
@@ -13,21 +13,27 @@
 void automated_setup(void) {
 
     /* init pins */
-    ledcAttachChannal(PWN_PIN, FRQ, PWM_BIT, CHN);
+    ledcSetup(CHN, FRQ, PWM_BIT);
+    ledcAttachPin(PWM_PIN, CHN);
     pinMode(EEP_PIN, OUTPUT);
     pinMode(RIGHT_MOTOR_PIN, OUTPUT);
-    pinMode(LEFT_MOTOR_PIN, OUTPUT); 
+    pinMode(LEFT_MOTOR_PIN, OUTPUT);
 
     /* Set init values */
-    digitalWrite(EEP_PIN,LOW);
+    digitalWrite(EEP_PIN,HIGH);
+    digitalWrite(LEFT_MOTOR_PIN,HIGH);
+    digitalWrite(RIGHT_MOTOR_PIN,LOW);
 }
 
 void motor_driver_PWM(void) {
+    ledcWrite(0, 255);
+    return;
     for (int i = 0; i < 255; i++) { //make light fade in
-        ledcWrite(PWN_PIN, i);
+        ledcWrite(0, i);
         delay(10); 
     }
     for (int i = 255; i > -1; i--) { 
+        ledcWrite(0, i);
         delay(10);
     } 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,14 @@
 #include <Arduino.h>
 #include <AutomatedResponse.h>
 
-#define PIN_LED    2
-#define PIN_BUTTON 13
-
 
 void setup() {
-  BL_Rx_Setup();
+  //BL_Rx_Setup();
   automated_setup();
+  
 
 }
 
 void loop() {
+  motor_driver_PWM();
 }
